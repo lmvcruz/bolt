c072b309f2c5bc5f16589f345c9003231a102e04
Leandro Cruz <unknown>
2023-05-16 09:45:51 +0100

    Add runner
    class: Runner
    Test: a test case using k-even sample was added

---
+++ bolt/runner.py
@@ -0,0 +1,30 @@
+from bolt.task import Task
+
+class Runner:
+    def __init__(self, task):
+        self.metrics = []
+        self.task = task
+        self.report = { "Metrics": {} }
+
+    def add_metric(self, metric):
+        self.metrics.append(metric)
+
+    def run(self, parameters: dict) -> dict:
+        self.setup()
+        exec_out = self.task.run(parameters)
+        self.report["ExecutionOutput"] = exec_out
+        self.teardown()
+        return self.report
+
+    def setup(self):
+        self.task.setup()
+        for m in self.metrics:
+            m.setup()
+
+    def teardown(self):
+        for m in self.metrics:
+            m.teardown(self.report["ExecutionOutput"])
+            self.report["Metrics"][m.name] = m.report
+        self.task.teardown()
+
+
---
+++ bolt/tests/runner_test.py
@@ -0,0 +1,15 @@
+from bolt.runner import Runner
+from bolt.metric import ExecutionTimeMetric
+
+from bolt.samples.k_even_single_exec.keventask import KEvenSingleExecutionTask
+
+def test_execution_time_metric_initialization():
+    metric = ExecutionTimeMetric()
+    task = KEvenSingleExecutionTask()
+    runner = Runner(task)
+    runner.add_metric(metric)
+    rep = runner.run({"k": 10})
+    print(rep)
+    exec_time = rep["Metrics"]["ExecutionTime"]["time"]
+    assert 0 < exec_time and exec_time < 1e-3
+    assert rep["ExecutionOutput"] == "0 2 4 6 8 10 12 14 16 18"
