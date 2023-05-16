75fcc8fc81e0d634a228198d4640581e5765b5b6
Leandro Cruz <unknown>
2023-05-16 09:09:24 +0100

    Add Metric
    classes: Metric (interface) and ExecutionTimeMetric (concrete)
    Add two tests for ExecutionTimeMetric

---
+++ bolt/tests/metric_test.py
@@ -0,0 +1,21 @@
+import pytest
+import time
+
+from bolt.metric import ExecutionTimeMetric
+
+def test_execution_time_metric_initialization():
+    m = ExecutionTimeMetric()
+    assert m.name == "ExecutionTime"
+    assert m.report == {}
+    assert m.start == None
+
+def test_execution_time_measurement():
+    m = ExecutionTimeMetric()
+    m.setup()
+    m.teardown()
+    assert 0 < m.report["time"] and m.report["time"] < 1.0e-3
+
+    m.setup()
+    time.sleep(1)
+    m.teardown()
+    assert m.report["time"] > 1
