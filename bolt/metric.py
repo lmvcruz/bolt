9bbbf46b2fc06133c905198688c21601fedca021
Leandro Cruz <>
2023-05-16 09:09:24 +0100

    Add Metric
    classes: Metric (interface) and ExecutionTimeMetric (concrete)
    Add two tests for ExecutionTimeMetric

---
+++ bolt/metric.py
@@ -0,0 +1,27 @@
+from abc import abstractmethod
+import time
+
+class Metric():
+    def __init__(self) -> None:
+        self.report = {}
+        self.name = None
+
+    @abstractmethod
+    def setup(self):
+        pass
+
+    @abstractmethod
+    def teardown(self, _=None):
+        pass
+
+class ExecutionTimeMetric(Metric):
+    def __init__(self) -> None:
+        super().__init__()
+        self.start = None
+        self.name = "ExecutionTime"
+
+    def setup(self):
+        self.start = time.time()
+
+    def teardown(self, _=None):
+        self.report["time"] = time.time() - self.start
