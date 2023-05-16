c9d9c5c79ea1bee4e6d1faebb4a480517db96dbd
Leandro Cruz <unknown>
2023-05-16 10:09:28 +0100

    Add NotImplementedError in abstract methods

--- bolt/tests/runner_test.py
+++ bolt/tests/runner_test.py
@@ -10,7 +10,6 @@ def test_execution_time_metric_initialization():
     runner = Runner(task)
     runner.add_metric(metric)
     rep = runner.run({"k": 10})
-    print(rep)
     exec_time = rep["Metrics"]["ExecutionTime"]["time"]
     assert 0 < exec_time and exec_time < 1e-3
     assert rep["ExecutionOutput"] == "0 2 4 6 8 10 12 14 16 18"
