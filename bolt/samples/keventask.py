482449ae6991ec5cd1faa9c5582fb664ff0c0cd5
Leandro Cruz <>
2023-05-17 11:43:06 +0100

    Add name attribute for in Task class

--- bolt/samples/keventask.py
+++ bolt/samples/keventask.py
@@ -3,7 +3,7 @@ from bolt.task import Task
 
 class KEvenSingleExecutionTask(Task):
     def __init__(self) -> None:
-        super().__init__()
+        super().__init__(KEvenSingleExecutionTask.__name__)
 
     def run(self, parameters: dict) -> str:
         return ' '.join([str(i*2) for i in range(int(parameters["k"]))])
